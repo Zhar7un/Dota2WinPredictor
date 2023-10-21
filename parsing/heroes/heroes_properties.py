740d57e0cd957c97bd424176cf449f99685f0659
Zhartun <gulyaevdima1998@gmail.com>
2023-10-21 11:47:18 +0300

    Add heroes package

---
+++ parsing/heroes/heroes_properties.py
@@ -0,0 +1,6 @@
+class HeroesProperties:
+    def _init_(self, *matches):
+        for match in matches:
+            self.additive_properties = None
+            self.counter_picks = None
+            self.synergy_picks = None
